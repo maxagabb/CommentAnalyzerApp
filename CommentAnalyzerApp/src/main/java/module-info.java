1c287f2a1457adc5490e23e31c7416b08438ed85
maxOrAng <maxOrAng@LAPTOP-95NUCTUP>
2019-04-16 15:24:59 -0400

    added module-info.java

---
+++ CommentAnalyzerApp/src/main/java/module-info.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+/**
+ * @author maxOrAng
+ *
+ */
+module commentAnalyzerApp {
+	exports byVideoFrontEnd;
+	exports byChannelFrontEnd;
+	exports loginRegister;
+	exports business;
+	exports api;
+	exports commentsFrontEnd;
+
+	requires com.fasterxml.jackson.core;
+	requires com.fasterxml.jackson.databind;
+	requires com.google.api.client;
+	requires com.google.common;
+	requires google.api.client;
+	requires google.api.services.youtube.v3.rev20190128;
+	requires google.http.client.jackson2;
+	requires google.oauth.client;
+	requires google.oauth.client.java6;
+	requires google.oauth.client.jetty;
+	requires java.desktop;
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires org.json;
+}
