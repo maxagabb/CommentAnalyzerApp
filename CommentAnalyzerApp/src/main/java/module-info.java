26e366e94b200b3be24fdff7dcd1081fb007d8ac
maxOrAng <maxOrAng@LAPTOP-95NUCTUP>
2019-04-24 09:19:51 -0400

    Added transitive to module-info.java

--- CommentAnalyzerApp/src/main/java/module-info.java
+++ CommentAnalyzerApp/src/main/java/module-info.java
@@ -13,20 +13,20 @@ module commentAnalyzerApp {
 	exports api;
 	exports commentsFrontEnd;
 
-	requires com.fasterxml.jackson.core;
-	requires com.fasterxml.jackson.databind;
-	requires com.google.api.client;
-	requires com.google.common;
-	requires google.api.client;
-	requires google.api.services.youtube.v3.rev20190128;
-	requires google.http.client.jackson2;
-	requires google.oauth.client;
-	requires google.oauth.client.java6;
-	requires google.oauth.client.jetty;
-	requires java.desktop;
-	requires javafx.base;
-	requires javafx.controls;
-	requires javafx.graphics;
-	requires org.json;
-	requires org.jsoup;
+	requires transitive com.fasterxml.jackson.core;
+	requires transitive com.fasterxml.jackson.databind;
+	requires transitive com.google.api.client;
+	requires transitive com.google.common;
+	requires transitive google.api.client;
+	requires transitive google.api.services.youtube.v3.rev20190128;
+	requires transitive google.http.client.jackson2;
+	requires transitive google.oauth.client;
+	requires transitive google.oauth.client.java6;
+	requires transitive google.oauth.client.jetty;
+	requires transitive java.desktop;
+	requires transitive javafx.base;
+	requires transitive javafx.controls;
+	requires transitive javafx.graphics;
+	requires transitive org.json;
+	requires transitive org.jsoup;
 }
